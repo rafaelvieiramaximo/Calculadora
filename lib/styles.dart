1e3e5862e8b8efc35d5623afa109d8a16f299a16
rafaelvieiramaximo <rafavmaximo@gmail.com>
2025-05-12 19:46:52 -0300

    Refactor theme data structure for light and dark modes

--- lib/styles.dart
+++ lib/styles.dart
@@ -4,5 +4,28 @@ class AppThemes {
   static final light = ThemeData(
     primarySwatch: Colors.indigo,
     brightness: Brightness.light,
+    scaffoldBackgroundColor: Colors.black,
+    textTheme: const TextTheme(
+      displayLarge: TextStyle(
+        color: Colors.white,
+        fontSize: 24,
+        fontWeight: FontWeight.bold,
+      ),
+      bodyLarge: TextStyle(color: Colors.white, fontSize: 16),
+    ),
+  );
+
+  static final dark = ThemeData(
+    primarySwatch: Colors.red,
+    brightness: Brightness.dark,
+    scaffoldBackgroundColor: Colors.black,
+    textTheme: const TextTheme(
+      displayLarge: TextStyle(
+        color: Colors.white,
+        fontSize: 24,
+        fontWeight: FontWeight.bold,
+      ),
+      bodyLarge: TextStyle(color: Colors.white, fontSize: 24),
+    ),
   );
 }
